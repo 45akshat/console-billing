004f08197de762f268b4d36974be7ec9bdf546a2
45akshat <akshatparmar78543@gmail.com>
2025-03-10 12:02:45 +0530

    whatsapp automation12

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -49,7 +49,7 @@ exports.createSession = async (req, res) => {
         updateCashInHand(user.Location_Id, sessionData.payment.cash);
 
 
-        sendTemplateMessage("91"+sessionData.primaryContact.contact, 'session_msg', 'session_msg', [
+        sendTemplateMessage("91"+sessionData.primaryContact.contact, 'session_msg2', 'session_msg2', [
             { name: 'total_price', value: sessionData.totalPrice }])
 
         // Send the response with the created session
