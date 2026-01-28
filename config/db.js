0beeb7024a6965a8686822840684b0526564ab09
Aman2292 <chaurasiyaaman110@gmail.com>
2026-01-29 00:04:31 +0530

    Production databse url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const  mongoose = require("mongoose")
 
 // const mongoURI = 'mongodb+srv://akshat:w065QdXmmL1EdFY8@cluster0.d4digua.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0';
 // const mongoURI = 'mongodb+srv://dbUser:sdsfascAADsa@cluster0.dr9bx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
- const mongoURI = 'mongodb+srv://slayde:7AQA67B1VeAvhMAp@cluster0.xycoh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true';
+ const mongoURI = 'mongodb+srv://dbUser:a2gQu0MMjHnlsAmO@cluster0.dr9bx.mongodb.net/Console-Production';
 
 //const mongoURI = "mongodb+srv://chaurasiyaaman2292:iNIvFBxH73jVWERF@attendance-cluster.kgvd3as.mongodb.net/test";
 const connectDb=()=>{
